4e55c60bb828a277ac53a36fe422ecfed1e80caa
Ericlys <107273056+EriclysLuckas@users.noreply.github.com>
2024-06-07 16:42:47 -0300

    at

--- ProjetoA3/telaUsuario/script.js
+++ ProjetoA3/telaUsuario/script.js
@@ -1,14 +1,48 @@
+// const serviceCards = document.querySelectorAll('.tipo');
+// const modal = document.getElementById('service-modal');
+// const modalTitle = document.getElementById('modal-title');
+// const serviceInput = document.getElementById('service-input');
+// const closeModal = document.querySelector('.close');
+
+// serviceCards.forEach(card => {
+//     card.addEventListener('click', () => {
+//         const service = card.getAttribute('data-service');
+//         modalTitle.innerText = `Agendar ${service}`;
+//         serviceInput.value = service;
+//         modal.style.display = 'flex';
+//     });
+// });
+
+// closeModal.addEventListener('click', () => {
+//     modal.style.display = 'none';
+// });
+
+// window.addEventListener('click', (event) => {
+//     if (event.target == modal) {
+//         modal.style.display = 'none';
+//     }
+// });
+
+// const form = document.getElementById('service-form');
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     const formData = new FormData(form);
+//     // Aqui você pode enviar os dados do formulário para o servidor
+//     console.log(Object.fromEntries(formData));
+//     modal.style.display = 'none';
+//     alert('Agendamento realizado com sucesso!');
+// });
+
+
 const serviceCards = document.querySelectorAll('.tipo');
 const modal = document.getElementById('service-modal');
 const modalTitle = document.getElementById('modal-title');
-const serviceInput = document.getElementById('service-input');
 const closeModal = document.querySelector('.close');
 
 serviceCards.forEach(card => {
     card.addEventListener('click', () => {
         const service = card.getAttribute('data-service');
         modalTitle.innerText = `Agendar ${service}`;
-        serviceInput.value = service;
         modal.style.display = 'flex';
     });
 });
@@ -21,14 +55,4 @@ window.addEventListener('click', (event) => {
     if (event.target == modal) {
         modal.style.display = 'none';
     }
-});
-
-const form = document.getElementById('service-form');
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const formData = new FormData(form);
-    // Aqui você pode enviar os dados do formulário para o servidor
-    console.log(Object.fromEntries(formData));
-    modal.style.display = 'none';
-    alert('Agendamento realizado com sucesso!');
 });
